6d8eb790a115465471f2d430472cbb08cec12c78
Matthew Viola <matthew.viola@mediware.com>
2024-02-28 19:08:13 -0500

    added API call from HTML

--- CodeFiles/app.py
+++ CodeFiles/app.py
@@ -2,8 +2,10 @@ import joblib
 import pandas as pd
 from flask import Flask, request
 from flask import make_response
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/askModel', methods=['POST'])
 def askModel():
@@ -11,14 +13,16 @@ def askModel():
     prediction = '0'
     if request.method == 'POST':
         
-        datafromjs = request.form['mydata']
-        X_test = pd.read_json(datafromjs, index=[0])
+        datafromjs = request.get_json()
+        print(datafromjs)
+        X_test = pd.DataFrame(datafromjs, index=[0])
 
         model = joblib.load("rf_model.pkl")
-        prediction = model.predict(X_test)[0]
+        prediction = str(model.predict(X_test)[0])
+        print(prediction)
         resp = make_response('{"response": '+prediction+'}')
         resp.headers['Content-Type'] = "application/json"
-        return resp
+        return prediction
 
 
 if __name__ == '__main__':
